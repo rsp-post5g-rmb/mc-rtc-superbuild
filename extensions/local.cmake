1862d63a6284dd802acc8758a73129b583d34d3b
Dhanushvarma <pdhanu2001@gmail.com>
2026-06-23 11:57:10 +0200

    add mc_triorb plugin

---
+++ extensions/local.cmake
@@ -0,0 +1,2 @@
+set(EXTENSIONS_DIR ${CMAKE_CURRENT_LIST_DIR}/superbuild-extensions)
+include(${CMAKE_CURRENT_LIST_DIR}/plugins/mc_triorb.cmake)
---
+++ extensions/plugins/mc_triorb.cmake
@@ -0,0 +1,5 @@
+AddProject(mc_triorb
+  GITHUB_PRIVATE rsp-post5g-rmb/mc_triorb
+  GIT_TAG origin/main
+  DEPENDS mc_rtc
+)
